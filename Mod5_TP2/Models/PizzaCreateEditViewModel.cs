7b402b0be6624c93a120cf4ce5fee25e642f25d4
chlorella84 <dominika.lecunff2019@campus-eni.fr>
2020-09-17 13:36:09 +0200

    pizza mniam

---
+++ Mod5_TP2/Models/PizzaCreateEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod5_TP2.Models
+{
+    public class PizzaCreateEditViewModel
+    {
+
+        public Pizza Pizza { get; set; }
+
+        public List<SelectListItem> Ingredients { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> Pates { get; set; } = new List<SelectListItem>();
+
+        public int IdPate { get; set; }
+        public List<int> IdSelectedIngredients { get; set; } = new List<int>();
+    }
+}
