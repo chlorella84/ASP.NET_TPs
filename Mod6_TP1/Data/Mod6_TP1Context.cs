d1b276c58f849875449cab9aef3bef3de18ce25b
chlorella84 <dominika.lecunff2019@campus-eni.fr>
2020-09-22 09:24:52 +0200

    debut mod6TP1

---
+++ Mod6_TP1/Data/Mod6_TP1Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mod6_TP1.Data
+{
+    public class Mod6_TP1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Mod6_TP1Context() : base("name=Mod6_TP1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<BO.Arme> Armes { get; set; }
+
+        public System.Data.Entity.DbSet<BO.Samourai> Samourais { get; set; }
+    }
+}
---
+++ Mod6_TP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mod6_TP1.MvcApplication" Language="C#" %>
